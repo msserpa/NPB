78fe73404a747de39279bce70e49b2388d458323
msserpa <msserpa@inf.ufrgs.br>
2019-04-06 20:16:02 -0300

    initial experiments

---
+++ NPB3.4-OMP/UA/npbparams.h
@@ -0,0 +1,39 @@
+c CLASS = C
+c  
+c  
+c  This file is generated automatically by the setparams utility.
+c  It sets the number of processors and the class of the NPB
+c  in this directory. Do not modify it by hand.
+c  
+        integer          lelt, lmor, refine_max, fre_default
+        integer          niter_default, nmxh_default
+        character        class_default
+        double precision alpha_default
+        parameter(  lelt=33500,
+     >             lmor=1262100,
+     >              refine_max=8,
+     >              fre_default=5,
+     >              niter_default=200,
+     >              nmxh_default=10,
+     >              class_default="C",
+     >              alpha_default=0.067d0 )
+        logical  convertdouble
+        parameter (convertdouble = .false.)
+        character compiletime*11
+        parameter (compiletime='02 Apr 2019')
+        character npbversion*3
+        parameter (npbversion='3.4')
+        character cs1*8
+        parameter (cs1='gfortran')
+        character cs2*5
+        parameter (cs2='$(FC)')
+        character cs3*6
+        parameter (cs3='(none)')
+        character cs4*6
+        parameter (cs4='(none)')
+        character cs5*12
+        parameter (cs5='-O3 -fopenmp')
+        character cs6*9
+        parameter (cs6='$(FFLAGS)')
+        character cs7*6
+        parameter (cs7='randi8')
