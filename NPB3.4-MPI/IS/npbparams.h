0b6b7ef8422c8e5ef5ef1a65d1515db447de1d97
msserpa <msserpa@inf.ufrgs.br>
2019-05-09 07:33:57 -0300

    remove ua

---
+++ NPB3.4-MPI/IS/npbparams.h
@@ -0,0 +1,14 @@
+#define CLASS 'C'
+/*
+   This file is generated automatically by the setparams utility.
+   It sets the number of processors and the class of the NPB
+   in this directory. Do not modify it by hand.   */
+   
+#define COMPILETIME "09 May 2019"
+#define NPBVERSION "3.4"
+#define MPICC "mpicc"
+#define CFLAGS "-O3"
+#define CLINK "$(MPICC)"
+#define CLINKFLAGS "$(CFLAGS)"
+#define CMPI_LIB "(none)"
+#define CMPI_INC "(none)"
